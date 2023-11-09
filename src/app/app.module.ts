a0b5e8753d883d3e33c5e2ab75bf3db98820ff1e
shivkumar1490 <124679555+shivkumar1490@users.noreply.github.com>
2023-11-09 15:47:47 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CityComponent } from './city/city.component';
+import {HighchartsChartModule} from 'highcharts-angular';
+@NgModule({
+  declarations: [
+    AppComponent,
+    CityComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HighchartsChartModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
